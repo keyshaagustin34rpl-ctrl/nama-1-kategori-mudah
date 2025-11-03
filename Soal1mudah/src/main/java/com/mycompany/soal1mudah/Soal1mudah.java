4f4f20e26dae9cecc45758d3fbf76e16bf2da724
keyshaagustin34rpl-ctrl <keysha_agustin_34rpl@student.smktelkom-mlg.sch.id>
2025-11-03 17:01:26 +0700

    Add files via upload

---
+++ Soal1mudah/src/main/java/com/mycompany/soal1mudah/Soal1mudah.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.soal1mudah;
+
+/**
+ *
+ * @author USER
+ */
+public class Soal1mudah {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
